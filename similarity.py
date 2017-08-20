230ac8aefa73986f3a0e3a2b8bd6456ecebe7914
David Hammer <d4vid.h4mm3r@gmail.com>
2017-08-20 16:20:06 +0200

    Endnu bedre og med civilstatus (WIP)

--- similarity.py
+++ similarity.py
@@ -36,7 +36,7 @@ def geo_dist(lat1, lon1, lat2, lon2):
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
     return R * c
 
-def geo_similarity(loc1, loc2, max_dist=6e5):
+def geo(loc1, loc2, max_dist=6e5):
     if loc1 == loc2:
         return 1
     if not (loc1 in _gps and loc2 in _gps):
@@ -45,15 +45,69 @@ def geo_similarity(loc1, loc2, max_dist=6e5):
     return max(0, 1 - dist/max_dist)
 
 # en string similarity (relativ til længste streng)
-def string_similarity_linear(a, b):
+def string_linear(a, b):
     d = editdistance.eval(a, b)
     return 1 - d / (max(len(a), len(b), 1))
 
 # en anden (omvendt proportional)
-def string_similarity_inverse(a,b):
+def string_inverse(a,b):
     d = editdistance.eval(a,b)
     return 1 / (1 + (d/(1 + len(a) + len(b))))
 
-def year_similarity(y1, y2, max_dist=4):
+def birthyear(y1, y2, max_dist=4):
     # lineært skalering med max
     return max(0, 1 - (abs(y1 - y2)/max_dist))
+
+ugift, gift, enke, fraskilt, forlovet = range(0, 5)
+ukendt = -1
+
+_follow_status = {
+    enke: {
+        gift: 0.5,
+        fraskilt: 0.1,
+        forlovet: 0.5
+    },
+    ugift: {
+        gift: 0.8,
+        forlovet: 1,
+        fraskilt: 0.5,
+        enke: 0.1
+    },
+    gift: {
+        fraskilt: 0.6,
+        enke: 0.8
+    },
+    fraskilt: {
+        forlovet: 0.3,
+        gift: 0.2
+    },
+    forlovet: {
+        gift: 0.9,
+        fraskilt: 0.2,
+        enke: 0.3
+    }
+}
+
+def parse_string(status):
+    if status[0:4].lower() == "enke":
+        return enke
+    elif status[0:4].lower() == "gift":
+        return gift
+    elif status[0:5].lower() == "ugift":
+        return ugift
+    elif status[0:6].lower() == "fraskil" or status[0:8].lower() == "separeret":
+        return fraskilt
+    elif status[0:7].lower() == "forlovet":
+        return forlovet
+    else:
+        return ukendt
+
+def status(status1, status2):
+    status1 = parse_string(status1)
+    status2 = parse_string(status2)
+    if status1 == -1 or status2 == -1:
+        return 0
+    elif status1 == status2:
+        return 1
+    elif status1 != status2:
+        return _follow_status[status1].get(status2, 0)
