ffc0a98bba0bb07eb4c491c6d1494bef48c52f1c
David Hammer <d4vid.h4mm3r@gmail.com>
2017-08-23 12:33:38 +0200

    Working on blocking by initials

--- utils.py
+++ utils.py
@@ -60,3 +60,15 @@ class AllEntries():
             next(fd) # skip header
             self.fds.add(fd)
             yield (fn, year, (parseEntry(line) for line in fd))
+
+_common_hard_c = set(("cristen", "cristian", "cristine", "carlotte", "catrine",
+                    "carl", "caroline", "cathrine", "cirstine", "carel", "catarine"))
+def initial_block(name):
+    # group k-like sounds with c into k
+    if name.startswith("c"):
+        if name in _common_hard_c or name.startswith("ch"):
+            return "k"
+    # group czs into c
+    if name[0] in "sz":
+        return "c"
+    return name[0]
