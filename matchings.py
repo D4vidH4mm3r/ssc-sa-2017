7e9d58184310ad5674e5c14b5e2c0dc1bdaabdb0
David Mortan Grøne Hammer <daham12@fe1.deic.sdu.dk>
2017-10-09 16:24:17 +0200

    Using some concurrency for computing graph

--- matchings.py
+++ matchings.py
@@ -3,6 +3,8 @@ import networkx as nx
 import pandas
 import sys
 import pathlib
+import concurrent.futures
+import utils
 
 
 print("Loading data for", sys.argv[1])
@@ -29,9 +31,6 @@ by_year = subset.groupby("FT")
 
 # Vil bruge edit distances til vægte - tager fødested $d_p$ og $d_n$ (ratio ens i stedet for direkte edit distance; længere navne kan have flere fejl; ratio går fra 0 til 1 hvor 1 betyder "ens").  Desuden tillader jeg op til 3 års fejl i angivelse af fødselsår (forskel $d_y$; her er det bare absolut forskel (mindre er bedre)).
 
-diff_name = difflib.SequenceMatcher()
-diff_place = difflib.SequenceMatcher()
-
 # Det hele er bare et eksperiment.  Nedenfor tilføjes kun kanter hvis $d_y \leq 3 \wedge d_n \geq 0.85$.  Vægte på kanter straffes for alle variabler, men måske med $d_n$ (problem: giver 0 hvis fødested er angivet helt forskelligt, men det kan være okay - se kommentarer til sidst).  Formel for vægt er lige nu:
 # $$w = d_n^2 \cdot d_p \cdot \frac{1}{1 + \frac{d_n}{3}}$$
 
@@ -42,32 +41,43 @@ b_year = 1850
 b_data = by_year.get_group(b_year)
 
 print("Building graph")
+
 G = nx.Graph()
-for a in a_data.itertuples():
-    diff_name.set_seq1(a.Navn)
-    diff_place.set_seq1(a.Fødested)
 
-    for b in b_data.itertuples():
-        # first filter really bad matches
-        age_diff = abs(a.Fødeår - b.Fødeår)
-        if age_diff > 3:
-            continue
+def doWork(G, chunk, b_data):
+    print("Staritng new chunk")
+    diff_name = difflib.SequenceMatcher()
+    diff_place = difflib.SequenceMatcher()
 
-        diff_name.set_seq2(b.Navn)
-        ratio_name = diff_name.ratio()
-        if ratio_name < 0.85:
-            continue
-        
-        diff_place.set_seq2(b.Fødested)
-        ratio_place = diff_place.ratio()
-        
-        # if maybe decent match, add edge
-        w = ratio_name**2 * ratio_place * 1/(1+age_diff/3)
-        G.add_edge(a.Index, b.Index, weight=w)
+    for a in chunk.itertuples():
+        diff_name.set_seq1(a.Navn)
+        diff_place.set_seq1(a.Fødested)
+
+        for b in b_data.itertuples():
+            # first filter really bad matches
+            age_diff = abs(a.Fødeår - b.Fødeår)
+            if age_diff > 3:
+                continue
+
+            diff_name.set_seq2(b.Navn)
+            ratio_name = diff_name.ratio()
+            if ratio_name < 0.85:
+                continue
+
+            diff_place.set_seq2(b.Fødested)
+            ratio_place = diff_place.ratio()
+
+            # if maybe decent match, add edge
+            w = ratio_name**2 * ratio_place * 1/(1+age_diff/3)
+            G.add_edge(a.Index, b.Index, weight=w)
+
+with concurrent.futures.ThreadPoolExecutor() as tpe:
+    for chunk in utils.chunks(a_data, 100):
+        tpe.submit(doWork, G, chunk, b_data)
 
 print("Finding matching")
-match = nx.max_weight_matching(G, maxcardinality=True)
-match = sorted(match.items(), key=lambda x: G[x[0]][x[1]]["weight"], reverse=True)
+match = nx.max_weight_matching(G, maxcardinality=False)
+match = sorted(set(tuple(sorted(item)) for item in match.items()), key=lambda x: G[x[0]][x[1]]["weight"], reverse=True)
 
 print("Writing it out")
 with fout.open("w") as fd:
