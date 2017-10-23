c7e34b9e0d4832df9757a3f3d734c81f3e3d5442
David Hammer <d4vid.h4mm3r@gmail.com>
2017-10-23 13:04:06 +0200

    Correct path and try fewer letters

--- scripts/split.py
+++ scripts/split.py
@@ -7,13 +7,13 @@ import metaphone
 print("Read pickled clean data")
 df = pd.read_pickle("dataframe.pickled")
 
-root = pathlib.Path("../work")
+root = pathlib.Path("/work/sdusscsa2/groups")
 
 header = "FT|Amt|Herred|Sogn|Navn|Køn|Fødested|Fødeår|Civilstand|Position|Erhverv|Kipnr|Løbenr".split("|")
 
 def make_keys(names):
     def f(s):
-        return metaphone.doublemetaphone(s.translate(utils.trans))[0][:3]
+        return metaphone.doublemetaphone(s.translate(utils.trans))[0][:2]
     return names.apply(f)
 
 print("Apply grouping function")
