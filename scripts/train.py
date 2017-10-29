ccc79cc70dcfdc8400be4c70d03fadf99e32b48f
David Hammer <d4vid.h4mm3r@gmail.com>
2017-10-29 17:23:18 +0100

    Trying to convert to scripts

---
+++ scripts/train.py
@@ -0,0 +1,60 @@
+from network import *
+import random
+import itertools
+
+
+matches = pd.read_csv(str(utils.datadir / "links" / "matches.csv"), sep="|")
+
+df = pd.read_pickle("dataframe.pickled")
+df.dropna(subset=("Fødeår",), inplace=True)
+df.set_index(["FT", "Kipnr", "Løbenr"], inplace=True)
+df.sort_index(inplace=True)
+
+def to_scored_frame(d):
+    lots = []
+    for t in d.itertuples():
+        try:
+            # TODO: optimize access
+            s = df.loc[[t[1:4], t[4:]]]
+            a = s.iloc[0]
+            b = s.iloc[1]
+        except KeyError:
+            continue
+        lots.append(score(a,b) + (True,))
+    return pd.DataFrame(lots, columns=score_columns + ["label"])
+
+years = [1845, 1850, 1860, 1880, 1885]
+
+ssize = 100
+def generate_nonmatch_scores(_):
+    lots = []
+    s = df.loc[random.choice(years)].sample(ssize)
+    for i in range(ssize-1):
+        a = s.iloc[i]
+        b = s.iloc[i+1]
+        lots.append(score(a,b) + (False,))
+    return lots
+
+with multiprocessing.Pool() as p:
+    res = p.map(generate_nonmatch_scores, [None for i in range(multiprocessing.cpu_count())])
+
+positive = utils.parallelize(matches, to_scored_frame)
+negative = pd.DataFrame(list(itertools.chain(*res)), columns=score_columns + ["label"])
+
+del(res)
+
+df_train = pd.concat([positive, negative])
+
+# # Train and evaluate models
+
+# ## Linear model
+input_fn_train = tf.estimator.inputs.pandas_input_fn(
+    x=df_train,
+    y=df_train.label,
+    shuffle=True)
+model1.train(input_fn=input_fn_train)
+print(model1.evaluate(input_fn=input_fn_train))
+
+# ## DNN model
+model2.train(input_fn_train)
+print(model2.evaluate(input_fn_train))
