4d23d3da6156351215ebc175a28fa033e26a7436
David Hammer <d4vid.h4mm3r@gmail.com>
2017-11-02 22:25:13 +0100

    More reshuffling

--- scripts/start-array-dir.sh
+++ scripts/start-array-dir.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 
 # This script will start program (arg 1) on all files in dir (arg 2)
-# This is done using job arrays (starts dirrunner.sh repeatedly)
+# This is done using job arrays (starts aux-runner-dir.sh repeatedly)
 
 # arg 1 should be program, arg 2 should be dir with many things in it
-
 shopt -s nullglob # so empty dirs count as 0
 maxarray=1000 # depends on slurm config; 1000 for Abacus
 
@@ -18,7 +17,7 @@ while [ $remaining -gt 0 ]; do
   dostart=$((remaining>maxarray?maxarray:remaining)) # how many to start now; take min
   echo "started: $numstarted, remaining: $remaining, now starting: $dostart"
   # running batch script thing takes dir, offset and goal script
-  sbatch --array=1-$((dostart)) dirrunner.sh $2 $numstarted $1
+  sbatch --array=1-$((dostart)) aux-runner-dir.sh $2 $numstarted $1
   remaining=$((remaining-dostart))
   numstarted=$((numstarted+dostart))
 done
