e24e8a7fce5dc847b00c7d340fa97a5122a13182
David Mortan Grøne Hammer <daham12@fe1.deic.sdu.dk>
2017-11-02 16:34:31 +0100

    Utility script for restarting

---
+++ scripts/checkifcancelled.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+for f in "$@"; do
+  grep "CANCELLED" $f -q
+  if [[ $? -eq 0 ]]; then
+    echo "$f was apparently cancelled"
+    s=$(head -n 1 $f)
+    fn=${s##* }
+    echo "Was working on $fn"
+    echo "$fn" >> remaining.csv
+  else
+    mv $f oldslurm/
+  fi
+done
