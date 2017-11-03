11ff4b45adc5d1f2d7ee0b209bf7238ae40fc86d
David Hammer <d4vid.h4mm3r@gmail.com>
2017-11-03 13:50:27 +0100

    Oh my god, this only takes a minute when done right

---
+++ scripts/4-nn-gen-score-pos.py
@@ -0,0 +1,46 @@
+#!/opt/sys/apps/python/3.6.0/bin/python
+
+from nn_common import *
+import random
+import itertools
+import utils
+import csv
+import concurrent.futures
+
+
+print("Loading golden standard")
+matches = pd.read_csv(str(utils.datadir / "links" / "matches.csv"), sep="|",
+    dtype={"a_FT": int, "a_Kipnr": str, "a_Løbenr": int, "b_FT": int, "b_Kipnr": str, "b_Løbenr": int},
+    comment="#")
+print("Loaded", len(matches), "matches in total")
+print("Loading big dataframe")
+df = pd.read_pickle("indexed.pickled")
+print("Loaded!")
+print("Converting to dict")
+d = {}
+for t in df.itertuples():
+    d[t.Index] = t 
+del(df)
+print("Done")
+
+def to_scored_frame(subset):
+    print("Starting on index", subset.index[0], flush=True)
+    res = []
+    for t in subset.itertuples():
+        a = t[1:4]
+        b = t[4:]
+        try:
+            a = d[a]
+            b = d[b]
+        except KeyError:
+            #print(a, "or", b, "not found")
+            continue
+        res.append(score(a,b) + (True,))
+    return res
+
+print("Starting the processing")
+with concurrent.futures.ProcessPoolExecutor(max_workers=48) as tpe:
+    data = tpe.map(to_scored_frame, utils.chunks(matches, 500))
+
+df = pd.DataFrame(list(itertools.chain(*data)), columns=score_columns+["labels"])
+df.to_csv(str(utils.workdir / "scores-positive.csv"))
