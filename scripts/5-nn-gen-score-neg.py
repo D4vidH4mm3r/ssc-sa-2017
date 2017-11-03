e015b00dfa0d09055409a7b9c19b3f47c03e37fd
David Hammer <d4vid.h4mm3r@gmail.com>
2017-11-03 20:47:17 +0100

    Simple computation of probably-not-matches

--- scripts/5-nn-gen-score-neg.py
+++ scripts/5-nn-gen-score-neg.py
@@ -3,24 +3,16 @@
 from nn_common import *
 import random
 import itertools
-import multiprocessing
+import concurrent.futures
 import sys
 
 
-print("NOT DONE YET - I HAVE TO GO CATCH A TRAIN D:")
-sys.exit()
-
-
 print("Loading big dataframe")
-df = pd.read_pickle("dataframe.pickled")
-print("Doing some indexing")
-df.dropna(subset=("Fødeår",), inplace=True)
-df.set_index(["FT", "Kipnr", "Løbenr"], inplace=True)
-df.sort_index(inplace=True)
+df = pd.read_pickle("indexed.pickled")
 
 years = [1845, 1850, 1860, 1880, 1885]
 
-ssize = 100
+ssize = 10000
 def generate_nonmatch_scores(_):
     lots = []
     s = df.loc[random.choice(years)].sample(ssize)
@@ -31,8 +23,8 @@ def generate_nonmatch_scores(_):
     return lots
 
 print("Compute scores for random non-matches")
-with multiprocessing.Pool() as p:
-    res = p.map(generate_nonmatch_scores, [None for i in range(multiprocessing.cpu_count())])
+with concurrent.futures.ProcessPoolExecutor(max_workers=48) as tpe:
+    data = tpe.map(generate_nonmatch_scores, [None for i in range(48)])
 
-negative = pd.DataFrame(list(itertools.chain(*res)), columns=score_columns + ["label"])
-del(res)
+df = pd.DataFrame(list(itertools.chain(*data)), columns=score_columns + ["label"])
+df.to_csv(str(utils.workdir / "scores-negative.csv"))
