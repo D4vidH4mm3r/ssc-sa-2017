1495bbdf2bc9d48cb9443c2c88bc923776191274
David Hammer <d4vid.h4mm3r@gmail.com>
2017-11-07 23:41:31 +0100

    Starting chunks of files from dir instead

--- scripts/aux-runner-dir.sh
+++ scripts/aux-runner-dir.sh
@@ -7,10 +7,17 @@
 # Batch file run from rundirarray.sh mostly
 # arg 1 should be dir with many files and stuff
 # arg 2 should be offset (stuff handled by other jobs)
+# arg 3 should be chunk size
 
-module load python/3.6.0
-files=($1/*)
-index=$(($2+SLURM_ARRAY_TASK_ID-1))
-file=${files[$index]}
-echo "$HOSTNAME runs $index, that is $file"
-$3 $file
+#module load python/3.6.0
+program=$1
+dir=$2
+chunksize=$3
+offset=$4
+
+files=($dir/*)
+startindex=$((offset+(SLURM_ARRAY_TASK_ID-1)*chunksize))
+subset=${files[@]:startindex:chunksize} # TODO: take min so no array OOB
+echo "Have: chunk size $chunksize"
+echo "$HOSTNAME runs from $startindex, that is $subset"
+$program $subset
--- scripts/start-array-dir.sh
+++ scripts/start-array-dir.sh
@@ -3,22 +3,30 @@
 # This script will start program (arg 1) on all files in dir (arg 2)
 # This is done using job arrays (starts aux-runner-dir.sh repeatedly)
 
-# arg 1 should be program, arg 2 should be dir with many things in it
+# arg 1 should be program
+# arg 2 should be dir with many things in it
+# arg 3 should be chunk size
 shopt -s nullglob # so empty dirs count as 0
 maxarray=1000 # depends on slurm config; 1000 for Abacus
 
-numfiles=($2/*)
+program=$1
+dir=$2
+chunksize=$3
+permaxarray=$((maxarray*chunksize))
+
+numfiles=($dir/*)
 numfiles=${#numfiles[@]}
+numarrays=$((numfiles/permaxarray))
+
 remaining=$numfiles
 numstarted=0
-
 echo "Will run $1 on $numfiles different inputs"
-while [ $remaining -gt 0 ]; do
-  dostart=$((remaining>maxarray?maxarray:remaining)) # how many to start now; take min
-  echo "started: $numstarted, remaining: $remaining, now starting: $dostart"
-  # running batch script thing takes dir, offset and goal script
-  sbatch --array=1-$((dostart)) aux-runner-dir.sh $2 $numstarted $1
-  remaining=$((remaining-dostart))
-  numstarted=$((numstarted+dostart))
+for (( i=0; i<= $numarrays; i++ )); do
+    dostart=$((remaining>permaxarray?permaxarray:remaining)) # how many to start now; take min
+    arraylen=$(((dostart+chunksize-1)/chunksize))
+    echo "Starting array number $i with $dostart files (will have array indices 1-$arraylen) and offset $numstarted"
+    sbatch --array=1-$arraylen aux-runner-dir.sh $program $dir $chunksize $numstarted
+    remaining=$((remaining-dostart))
+    numstarted=$((numstarted+dostart))
 done
 
