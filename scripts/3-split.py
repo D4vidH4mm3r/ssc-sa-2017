13b9909377361b631bc79fb48117adad0c5f7a27
David Hammer <d4vid.h4mm3r@gmail.com>
2017-11-08 15:50:46 +0100

    Three-level splitting

--- scripts/3-split.py
+++ scripts/3-split.py
@@ -11,24 +11,33 @@ root = pathlib.Path("/work/sdusscsa2/groups")
 
 header = "FT|Amt|Herred|Sogn|Navn|Køn|Fødested|Fødeår|Civilstand|Position|Erhverv|Kipnr|Løbenr".split("|")
 
-def make_keys(names):
-    def f(s):
-        return metaphone.doublemetaphone(s.translate(utils.trans))[0][:2]
-    return names.apply(f)
-
 print("Apply grouping function")
-df["G1"] = df.FonetiskFornavn.apply(lambda x: x[0])
-df["G2"] = df.FonetiskFornavn.apply(lambda x: x[1])
+df["G1"] = df.FonetiskFornavn.apply(lambda x: x[0][:2].replace(" ", "_"))
+df["G2"] = df.FonetiskFornavn.apply(lambda x: x[0][:4].replace(" ", "_"))
+df["G3"] = df.FonetiskFødested.apply(lambda x: x[0][:2].replace(" ", "_"))
 
+def savegroup(block, data):
+    fn = root / ("-".join(block) + ".csv")
+    print("Write out group", fn.stem)
+    if fn.exists():
+        data.to_csv(str(fn), header=False, mode="a")
+    else:
+        data.to_csv(str(fn), header=True, mode="a")
+
+LIMIT = 50000
 print("Do the groupby and write")
-for g in ("G1", "G2"):
-    for (a, b), data in df.groupby(["Køn", g]):
-        if b == "":
-            continue
-        gender = "f" if a else "m"
-        fn = root / (gender + "-" + b + ".csv")
-        print("Write out group", (a,b))
-        if fn.exists():
-            data.to_csv(str(fn), header=False, mode="a")
-        else:
-            data.to_csv(str(fn), header=True, mode="a")
+for (gender, prefix1), data1 in df.groupby(["Køn", "G1"]):
+    gender = "f" if gender else "m"
+
+    print((gender, prefix1), "has size", len(data1))
+    if len(data1) <= LIMIT:
+        savegroup((gender, prefix1), data1)
+    else: # split further
+        for prefix2, data2 in data1.groupby(["G2"]):
+            print(" ", prefix2, "has size", len(data2))
+            if len(data2) <= LIMIT:
+                savegroup((gender, prefix1, prefix2), data2)
+            else: # split even further
+                for prefix3, data3 in data2.groupby(["G3"]):
+                    print("   ", prefix3, "has size", len(data3))
+                    savegroup((gender, prefix1, prefix2), data2)
