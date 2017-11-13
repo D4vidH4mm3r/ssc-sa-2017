a4561db4852dc85a31a1239d0589a8a978df67a0
David Hammer <d4vid.h4mm3r@gmail.com>
2017-11-13 16:16:06 +0100

    Prefix slurm again

--- scripts/start-python.sh
+++ scripts/start-python.sh
@@ -1,4 +1,4 @@
 program=$1
 simple="${program%.*}" # strips extension
-simple="${simple#*-}" # strips leading n-
-sbatch -o "${simple}.output" -J "$simple" aux-runner-python.job "$program"
+#simple="${simple#*-}" # strips leading n-
+sbatch -o "slurm-${simple}.out" -J "$simple" aux-runner-python.job "$program"
