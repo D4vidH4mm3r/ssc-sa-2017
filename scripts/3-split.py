a769e3868952fcc381b3565e619bfecd95a02d4e
David Hammer <d4vid.h4mm3r@gmail.com>
2017-11-14 17:30:01 +0100

    Using both metaphone groups again

--- scripts/3-split.py
+++ scripts/3-split.py
@@ -11,11 +11,6 @@ root = pathlib.Path("/work/sdusscsa2/groups")
 
 header = "FT|Amt|Herred|Sogn|Navn|Køn|Fødested|Fødeår|Civilstand|Position|Erhverv|Kipnr|Løbenr".split("|")
 
-print("Apply grouping function")
-df["G1"] = df.FonetiskFornavn.apply(lambda x: x[0][:2].replace(" ", "_"))
-df["G2"] = df.FonetiskFornavn.apply(lambda x: x[0][:4].replace(" ", "_"))
-df["G3"] = df.FonetiskFødested.apply(lambda x: x[0][:2].replace(" ", "_"))
-
 def savegroup(block, data):
     fn = root / ("-".join(block) + ".csv")
     print("Write out group", fn.stem)
@@ -24,20 +19,33 @@ def savegroup(block, data):
     else:
         data.to_csv(str(fn), header=True, mode="a")
 
-LIMIT = 50000
-print("Do the groupby and write")
-for (gender, prefix1), data1 in df.groupby(["Køn", "G1"]):
-    gender = "f" if gender else "m"
+def write_all(df):
+    LIMIT = 50000
+    print("Do the groupby and write")
+    for (gender, prefix1), data1 in df.groupby(["Køn", "G1"]):
+        gender = "f" if gender else "m"
 
-    print((gender, prefix1), "has size", len(data1))
-    if len(data1) <= LIMIT:
-        savegroup((gender, prefix1), data1)
-    else: # split further
-        for prefix2, data2 in data1.groupby(["G2"]):
-            print(" ", prefix2, "has size", len(data2))
-            if len(data2) <= LIMIT:
-                savegroup((gender, prefix1, prefix2), data2)
-            else: # split even further
-                for prefix3, data3 in data2.groupby(["G3"]):
-                    print("   ", prefix3, "has size", len(data3))
-                    savegroup((gender, prefix1, prefix2, prefix3), data3)
+        print((gender, prefix1), "has size", len(data1))
+        if len(data1) <= LIMIT:
+            savegroup((gender, prefix1), data1)
+        else: # split further
+            for prefix2, data2 in data1.groupby(["G2"]):
+                print(" ", prefix2, "has size", len(data2))
+                if len(data2) <= LIMIT:
+                    savegroup((gender, prefix1, prefix2), data2)
+                else: # split even further
+                    for prefix3, data3 in data2.groupby(["G3"]):
+                        print("   ", prefix3, "has size", len(data3))
+                        savegroup((gender, prefix1, prefix2, prefix3), data3)
+
+print("Apply grouping function")
+df["G1"] = df.FonetiskFornavn.apply(lambda x: x[0][:2].replace(" ", "_"))
+df["G2"] = df.FonetiskFornavn.apply(lambda x: x[0][:4].replace(" ", "_"))
+df["G3"] = df.FonetiskFødested.apply(lambda x: x[0][:2].replace(" ", "_"))
+print("Writing out so much stuff")
+write_all(df)
+print("Apply different grouping function")
+df["G1"] = df.FonetiskFornavn.apply(lambda x: x[1][:2].replace(" ", "_"))
+df["G2"] = df.FonetiskFornavn.apply(lambda x: x[1][:4].replace(" ", "_"))
+df["G3"] = df.FonetiskFødested.apply(lambda x: x[1][:2].replace(" ", "_"))
+write_all(df)
