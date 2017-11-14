18eb3a6225beb14109e98d16e2869ca338dc4094
David Hammer <d4vid.h4mm3r@gmail.com>
2017-11-14 17:22:00 +0100

    Cleanup

--- scripts/8-extract-matches.py
+++ scripts/8-extract-matches.py
@@ -27,65 +27,19 @@ def process(fn):
     matches = uniqueify_max(matches)
     return matches
 
-with concurrent.futures.ProcessPoolExecutor(max_workers=10) as tpe:
-    res = tpe.map(process, pathlib.Path("nn-match").glob("*.csv"))
+with concurrent.futures.ProcessPoolExecutor() as tpe:
+    res = tpe.map(process, (utils.workdir / "nn-match").glob("*.csv"))
 
-res = list(res)
-
-len(res)
-
-matches = pd.concat(res)
-
+matches = pd.concat(list(res))
 del(res)
+print(len(matches), "matches remaining")
 
-len(matches)
-
-# so how many left?
-g1 = matches.groupby(by=["a_FT", "a_Kipnr", "a_Løbenr"], as_index=False)
-g2 = matches.groupby(by=["b_FT", "b_Kipnr", "b_Løbenr"])
-l1=list(g1.size().groupby(level=0).size().iteritems())
-l2=list(g2.size().groupby(level=0).size().iteritems())
-
-for (a_year, a_count), (b_year, b_count) in zip(l1, l2):
-    print(a_year, b_year, min(a_count, b_count))
-print(sum(min(a, b) for ((_, a), (_, b)) in zip(l1,l2)))
-
-g = matches.groupby("a_FT")
-
-s = 0
-for a_year, size in g.size().iteritems():
-    print(a_year, size)
-    s += size
-s
-
-df = pd.read_pickle("../scripts/dataframe.pickled")
-
+df = pd.read_pickle("dataframe.pickled")
 g2 = df.groupby("FT")
-
 l = list(g2.size().iteritems())
-
 for (a_year, a_size), (b_year, b_size), (our_year, our_size) in zip(l, l[1:], g.size().iteritems()):
     ul = min(a_size, b_size)
     print(a_year, "->", b_year, our_size, "/", ul, our_size/ul)
 
 hmm = matches.p.describe()
-
 print(hmm.to_latex())
-
-# ## There are probably still duplicates
-# Obvious if `len(matches)` above is greater than the sum in the cell directly above
-
-indexed = matches.set_index(["a_FT", "a_Kipnr", "a_Løbenr"])
-
-d = indexed.index.get_duplicates()
-
-indexed.loc[d]
-
-len(matches)
-
-matches.drop_duplicates(subset=["a_FT", "a_Løbenr", "a_Kipnr"], inplace=True)
-matches.drop_duplicates(subset=["b_FT", "b_Løbenr", "b_Kipnr"], inplace=True)
-
-len(matches)
-
-matches
